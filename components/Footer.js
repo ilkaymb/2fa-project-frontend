f0234f4b21ae65a914fbb11d0dc66532d2e2b22f
ilkaymb <ilkaymb@hotmail.com>
2024-02-23 03:05:41 +0300

    Add Footer component

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="bg-indigo-600">
+      {" "}
+      <div className="container mx-auto flex justify-center text-white text-xl py-14">
+        Created By İlkay Mehmet Bora
+      </div>
+    </div>
+  );
+}
