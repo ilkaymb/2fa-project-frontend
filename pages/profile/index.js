2a53ff79da472e9b4fd9ebd3c9b5db76c21f1625
ilkaymb <ilkaymb@hotmail.com>
2024-02-23 04:42:26 +0300

    Add PrivateRoute, Header, and Footer components to profile page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { Inter } from "next/font/google";
+import PrivateRoute from "@/components/PrivateRoute";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function index() {
   return (
-    <main className={`${inter.className}`}>
-      <div className="bg-indigo-950 text-white h-[100vh] flex justify-center items-center">
-        <div className="bg-indigo-800 border border-indigo-600 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-30 relative transition-all duration-200">
-          <h1 className="text-4xl text-white font-bold text-center ">
-            Profile Page
-          </h1>
-        </div>{" "}
-      </div>
-    </main>
+    <PrivateRoute tokenName="STEP2_TOKEN">
+      <Header />
+      <main className={`${inter.className}`}>
+        <div className="bg-indigo-950 text-white h-[100vh] flex justify-center items-center">
+          <div className="bg-indigo-800 border border-indigo-600 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-30 relative transition-all duration-200">
+            <h1 className="text-4xl text-white font-bold text-center ">
+              Profile Page
+            </h1>
+          </div>{" "}
+        </div>
+      </main>
+      <Footer />
+    </PrivateRoute>
   );
 }
