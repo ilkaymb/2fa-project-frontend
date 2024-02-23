0f92e2503935fda4a03b2af9263220130f552b41
ilkaymb <ilkaymb@hotmail.com>
2024-02-23 04:42:12 +0300

    Add PrivateRoute component and update cookie management

--- pages/verifyOTP/index.js
+++ pages/verifyOTP/index.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import { useRouter } from "next/router";
 const inter = Inter({ subsets: ["latin"] });
 import { useCookies } from "react-cookie";
+import PrivateRoute from "@/components/PrivateRoute";
 
 export default function Home() {
   const [otp, setOtp] = useState("");
@@ -13,7 +14,10 @@ export default function Home() {
 
   const router = useRouter();
 
-  const [cookies, setCookie, removeCookie] = useCookies(["jwtToken"]); // userToken adında bir cookie yönetmek istiyorsunuz
+  const [cookies, setCookie, removeCookie] = useCookies([
+    "STEP1_TOKEN",
+    "STEP2_TOKEN",
+  ]); // userToken adında bir cookie yönetmek istiyorsunuz
 
   const verifyOTP = async (e) => {
     try {
@@ -22,12 +26,18 @@ export default function Home() {
         "http://localhost:3001/api/auth/verifyOTP",
         {
           otp: otp,
-          jwtToken: cookies.jwtToken,
+          STEP1_TOKEN: cookies.STEP1_TOKEN,
         }
       );
 
       // OTP doğrulandı
       setIsVerified(true);
+
+      alert(response.data.message);
+      let token = response.data.token;
+      console.log(token);
+      setCookie("STEP2_TOKEN", token, { path: "/", maxAge: 3600 }); // Cookie olarak kaydet
+      removeCookie("STEP1_TOKEN");
       setError(""); // Hata mesajını temizle
       router.push("/profile");
 
@@ -42,37 +52,40 @@ export default function Home() {
 
   return (
     <main className={`${inter.className}`}>
-      <div className="bg-indigo-950 text-white h-[100vh] flex justify-center items-center">
-        <div className="bg-indigo-900 border border-indigo-600 rounded-md px-6 py-8 shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-30 relative transition-all duration-200">
-          <form className="otp-form w-full sm:w-[300px] md:w-[300px] lg:w-[300px]">
-            <h1 className="text-2xl text-center mb-4 font-bold capitalize">
-              OTP has been sent to your email
-            </h1>
-            <input
-              type="text"
-              value={otp}
-              onChange={(e) => setOtp(e.target.value)}
-              placeholder="Enter OTP"
-              required
-              className="w-full p-3  mb-6 rounded-t-md  text-black  border-0 border-b-2 border-white appearance-none dark:text-black dark:border-white dark:focus:border-indigo-500 focus:outline-none focus:right-0 focus:text-black focus:border-indigo-600 peer"
-            />
-            <p className="text-sm text-gray-200">
-              {" "}
-              *Please check your spam folder if you don{"'"}t see the email in
-              your inbox.
-            </p>
+      {" "}
+      <PrivateRoute tokenName="STEP1_TOKEN">
+        <div className="bg-indigo-950 text-white h-[100vh] flex justify-center items-center">
+          <div className="bg-indigo-900 border border-indigo-600 rounded-md px-6 py-8 shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-30 relative transition-all duration-200">
+            <form className="otp-form w-full sm:w-[300px] md:w-[300px] lg:w-[300px]">
+              <h1 className="text-2xl text-center mb-4 font-bold capitalize">
+                OTP has been sent to your email
+              </h1>
+              <input
+                type="text"
+                value={otp}
+                onChange={(e) => setOtp(e.target.value)}
+                placeholder="Enter OTP"
+                required
+                className="w-full p-3  mb-6 rounded-t-md  text-black  border-0 border-b-2 border-white appearance-none dark:text-black dark:border-white dark:focus:border-indigo-500 focus:outline-none focus:right-0 focus:text-black focus:border-indigo-600 peer"
+              />
+              <p className="text-sm text-gray-200">
+                {" "}
+                *Please check your spam folder if you don{"'"}t see the email in
+                your inbox.
+              </p>
 
-            <button
-              type="button"
-              className="w-full mb-4 text-[18px] mt-6 rounded-lg bg-white text-indigo-900 hover:bg-indigo-600 hover:text-white py-2 transition-colors duration-300"
-              onClick={verifyOTP}
-            >
-              Verify OTP
-            </button>
-            {error && <p className="error-msg">{error}</p>}
-          </form>
-        </div>
-      </div>
+              <button
+                type="button"
+                className="w-full mb-4 text-[18px] mt-6 rounded-lg bg-white text-indigo-900 hover:bg-indigo-600 hover:text-white py-2 transition-colors duration-300"
+                onClick={verifyOTP}
+              >
+                Verify OTP
+              </button>
+              {error && <p className="error-msg">{error}</p>}
+            </form>
+          </div>
+        </div>{" "}
+      </PrivateRoute>
     </main>
   );
 }
