8f5c0da731e57da0143ab1931e0bfd775b6901d8
ilkaymb <ilkaymb@hotmail.com>
2024-02-26 08:22:18 +0300

    Add token verification using Axios

--- components/Step2PrivateRoute.js
+++ components/Step2PrivateRoute.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useCookies } from "react-cookie";
+import axios from "axios"; // Axios'u import et
 
 const Step2PrivateRoute = ({ children }) => {
   const [cookies] = useCookies(["STEP2_TOKEN"]); // Dinamik cookie ismi
@@ -10,14 +11,37 @@ const Step2PrivateRoute = ({ children }) => {
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    // Component mount edildiğinde client-side olduğunu belirle
-    setIsClient(true);
+    const verifyToken = async () => {
+      try {
+        // verifyUser API endpoint'ine istek at
+        const response = await axios.post(
+          "http://localhost:3001/api/auth/verifyUser",
+          {
+            token: cookies["STEP2_TOKEN"],
+          }
+        );
+        // Başarılı yanıt alınırsa, token geçerlidir
+        if (response.status === 200) {
+          alert("Token doğrulandı");
+          setIsClient(true);
+        } else {
+          console.error(response.status);
 
-    // Dinamik olarak belirlenen cookie yoksa ve client-side'daysak, ana sayfaya yönlendir
-    if (!cookies["STEP2_TOKEN"]) {
-      router.push("/");
+          router.push("/profile");
+        }
+      } catch (error) {
+        console.error("Token doğrulama hatası:", error);
+        removeCookie("STEP1_TOKEN");
+
+        router.push("/"); // Hata durumunda ana sayfaya yönlendir
+      }
+    };
+
+    // Cookie varsa ve client-side'daysak, token'ı doğrula
+    if (cookies["STEP2_TOKEN"]) {
+      verifyToken();
     }
-  }, [cookies, router, isClient]); // tokenName ve isClient'i bağımlılıklara ekle
+  }, []);
 
   // Client-side kontrolü sağlamak ve server-side render ile uyumsuzluk olmamasını sağlamak için isClient kontrolü
   if (!isClient) {
